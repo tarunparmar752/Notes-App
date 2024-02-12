3d6872ad27625831b3543dc4610301721c3a352d
Tarun Parmar <tarunparmar752@gmail.com>
2024-02-12 19:22:09 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+import fs from 'fs'
+import chalk from 'chalk';
+
+ //fs.writeFileSync('notes.txt',' file was created by Node.js!')
+//  fs.appendFileSync('notes.txt','Tarun')
+console.log(chalk.red.bold('Hiiiii'))
